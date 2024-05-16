33523562aa6f7279a08592e5a107eddf82602e6c
HarleyMorales <harleymoraa6@gmail.com>
2024-05-16 00:50:13 -0400

    Added nested roots and rendering child elements on the right handside

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 
 
 const router = createBrowserRouter([
@@ -11,6 +12,12 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
